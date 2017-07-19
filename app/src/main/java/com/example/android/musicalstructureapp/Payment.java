721fa1e05a51ec263134eae2a2073d5b97996f9a
Vicuko <victorc.fernandez@gmail.com>
2017-07-20 00:53:07 +0200

    Define Payment.java file to include logic for screen navigation

--- app/src/main/java/com/example/android/musicalstructureapp/Payment.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Payment.java
@@ -1,16 +1,35 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Payment extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        Button toLibrary = (Button) findViewById(R.id.payment_library);
+        Button toLibrary2 = (Button) findViewById(R.id.payment_library2);
+
+        toLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Payment.this, SearchLibrary.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        toLibrary2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Payment.this, SearchLibrary.class);
+                startActivity(loginIntent);
+            }
+        });
 
     }
 }
