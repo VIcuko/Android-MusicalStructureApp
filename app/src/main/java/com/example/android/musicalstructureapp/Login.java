73148f410366d72e876f610517721223d36cfcb5
Vicuko <victorc.fernandez@gmail.com>
2017-07-20 00:39:40 +0200

    Inform Login.java to create intent on button for login

--- app/src/main/java/com/example/android/musicalstructureapp/Login.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Login.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        Button login = (Button) findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Login.this, SearchLibrary.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
