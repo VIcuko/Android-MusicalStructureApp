4177ef4d48d80932be7af2d9b1fd83f57d7e2e92
Vicuko <victorc.fernandez@gmail.com>
2017-07-20 00:46:31 +0200

    Inform SearchLibrary.java file with links to corresponding views

--- app/src/main/java/com/example/android/musicalstructureapp/SearchLibrary.java
+++ app/src/main/java/com/example/android/musicalstructureapp/SearchLibrary.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchLibrary extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class SearchLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_library);
+        Button playSong = (Button) findViewById(R.id.play_song);
+        Button buySong = (Button) findViewById(R.id.buy_song);
+        Button playingNow = (Button) findViewById(R.id.playing_now);
+        Button map = (Button) findViewById(R.id.map);
+
+
+        playSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(SearchLibrary.this, Playing.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        buySong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(SearchLibrary.this, Payment.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        playingNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(SearchLibrary.this, Playing.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(SearchLibrary.this, MusicMap.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
