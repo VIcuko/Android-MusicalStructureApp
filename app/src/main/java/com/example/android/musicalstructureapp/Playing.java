3f3400808ad72127b903cef87701532119eaae53
Vicuko <victorc.fernandez@gmail.com>
2017-07-20 00:50:29 +0200

    Inform playing.java file with logic for screen navigation

--- app/src/main/java/com/example/android/musicalstructureapp/Playing.java
+++ app/src/main/java/com/example/android/musicalstructureapp/Playing.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Playing extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class Playing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing);
+        Button toLibrary = (Button) findViewById(R.id.playing_library);
+        Button toMap = (Button) findViewById(R.id.playing_map);
+
+        toLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Playing.this, SearchLibrary.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        toMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Playing.this, MusicMap.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
